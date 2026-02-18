4fc6c976505b5d01e9b90e122e564060bd6eeb90
abhinavpuranik <abhunav.puranik@gmail.com>
2026-02-18 15:14:18 +0530

    Coded UC2 to Print OOPS to Console using string concatenation via +

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+// @author developer
+// version 2
+
+public class OOPSBannerApp {
+
+	public static void main(String[] args) {
+		System.out.println("******" + " " + "******" + " " + "******" + " " + "******");   
+		System.out.println("*    *" + " " + "*    *" + " " + "*    *" + " " + "*     ");
+		System.out.println("*    *" + " " + "*    *" + " " + "******" + " " + "******");
+		System.out.println("*    *" + " " + "*    *" + " " + "*     " + " " + "     *");
+		System.out.println("******" + " " + "******" + " " + "*     " + " " + "****** ");
+		
+	
+	}
+
+}
