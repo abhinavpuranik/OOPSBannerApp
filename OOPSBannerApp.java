e9ce8e747aa89d9655e7893787f62212d1a14991
abhinavpuranik <abhunav.puranik@gmail.com>
2026-02-19 13:49:15 +0530

    Coded UC7 to Print OOPS to console using inner class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,19 @@
-public class OOPSBannerApp{
-	public static void main(String[] args){
-		
-		String[] oPattern=getOPattern();
-		String[] pPattern=getPPattern();
-		String[] sPattern=getSPattern();
-		for(int i=0;i<oPattern.length;i++){
-			System.out.println(oPattern[i]+" "+oPattern[i]+" "+pPattern[i]+" "+sPattern[i]);
+public class OOPSBannerApp {
+	static class CharacterPatternMap{
+		char character;
+		String[] pattern;
+
+		CharacterPatternMap(char character, String[] pattern) {
+			this.character = character;
+			this.pattern = pattern;
+
 		}
 	}
-	public static String[] getOPattern(){
-		return new String[]{
+
+	public static CharacterPatternMap[] createCharacterPatternMaps() {
+		OOPSBannerApp.CharacterPatternMap[] charMap = new OOPSBannerApp.CharacterPatternMap[26];
+
+		charMap[14] = new CharacterPatternMap('O', new String[] {
 			"   ***   ",
 			" **   ** ",
 			"**     **",
@@ -17,10 +21,9 @@ public class OOPSBannerApp{
 			"**     **",
 			" **   ** ",
 			"   ***   "
-		};
-	}
-	public static String[] getPPattern(){
-		return new String[]{
+		});
+
+		charMap[15] = new CharacterPatternMap('P', new String[] {
 			"******   ",
 			"**    ** ",
 			"**     **",
@@ -28,10 +31,8 @@ public class OOPSBannerApp{
 			"******   ",
 			"**       ",
 			"**       "
-		};
-	}
-	public static String[] getSPattern(){
-		return new String[]{
+		});
+		charMap[18] = new CharacterPatternMap('S', new String[] {
 			"   ***** ",
 			" **      ",
 			"**       ",
@@ -39,6 +40,42 @@ public class OOPSBannerApp{
 			"     **  ",
 			"**    ** ",
 			"  ****   "
-		};
+		});
+
+		return charMap;
+		
 	}
+
+	public static String[] getCharacterPattern(char ch, CharacterPatternMap[] charMap) {
+		return charMap[ch - 'A'].pattern;
+	}
+
+	public static void printMessage(String message, CharacterPatternMap[] charMap) {
+
+		int len = message.length();
+		String[][] ans = new String[len][7];
+		int i = 0;
+		for(char ch:message.toCharArray()) {
+			ans[i++] = getCharacterPattern(ch, charMap);
+
+		}
+
+		for(int j = 0; j < 7; j++) {
+			for(int k = 0; k < len; k++) {
+				System.out.print(ans[k][j]);
+				System.out.print(" ");
+
+			}
+			System.out.println();
+
+		}
+		
+
+	}
+	public static void main(String[] args) {
+			CharacterPatternMap[] charMap = createCharacterPatternMaps();
+			String message = "OOPS";
+			printMessage(message,charMap);
+
+		}
 }
